a3c0db3c78470d642cc39a71a9835578ea1bc4d3
Alvin Kaburu <geekaburu@gmail.com>
2017-05-16 00:59:00 +0300

    Made final tweaks

--- database/seeds/ProfilesTableSeeder.php
+++ database/seeds/ProfilesTableSeeder.php
@@ -11,7 +11,18 @@ class ProfilesTableSeeder extends Seeder
      */
     public function run()
     {
+        // Create a specific test user
+        App\Profile::create([
+            'user_id'=>1,
+            'name' => "John Doe",
+            'email' => "doe@joe.com",
+            'national_id'=>"987654321" ,
+            'mobile_number'=> "0712345678",
+            'city' => "Nairobi",
+            'country' => "Kenya",
+        ]);
+
         // Create profile records
-        factory(App\Profile::class, 50)->create();
+        factory(App\Profile::class, 49)->create();
     }
 }
